aa1e29df2739fb1b5c9bd7e7ac3516e403e48981
tanizawa_kenta <kt10025102@gmail.com>
2020-01-22 21:23:26 +0900

    リファクタリング後

---
+++ README.md
@@ -0,0 +1,26 @@
+# VendingMachine
+# How to use
+
+$ irb
+\> require './VendingMachine3.rb'
+\> vm = VendingMachine.new
+\> vm.slot_money (10) => 10
+\> vm.slot_money (50) => 60
+\> vm.slot_money (100) => 160
+\> vm.slot_money (500) => 660
+\> vm.slot_money (1000) => 1660
+\> vm.slot_money (1) => false
+\> vm.current_slot_money => 1660
+\> vm.current_stock =>  {"cola"=>5, "redbull"=>5, "water"=>5}
+\> vm.drink_check(Drink.cola) => colaを購入できます。
+\> vm.drink_check(Drink.redbull) =>redbullを購入できます。
+\> vm.drink_check(Drink.water) =>waterを購入できます。
+\> vm.buy_drink(Drink.cola) =>colaを購入しました。残高は1540円です。
+\> vm.buy_drink(Drink.redbull) =>redbullを購入しました。残高は1340円です。
+\> vm.buy_drink(Drink.water) =>waterを購入しました。残高は1240円です。
+\> vm.return_money => 1240
+\> vm.sale_amount => 420
+\> vm.drink_check(Drink.cola) =>colaを購入できませんでした。
+\> vm.buy_drink(Drink.cola) =>colaを購入できません。
+\> vm.current_stock =>  {"cola"=>4, "redbull"=>4, "water"=>4}
+\> exit
--- VendingMachine3.rb
+++ VendingMachine3.rb
@@ -17,32 +17,32 @@
 # vm.return_money
 
 class Drink
-  attr_reader :name, :price
+  attr_accessor :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 
   def self.cola
-    self.new(:cola, 120)
+    self.new("cola", 120)
   end
 
   def self.redbull
-    self.new(:redbull, 200)
+    self.new("redbull", 200)
   end
 
   def self.water
-    self.new(:water, 100)
-  end
-
-  def initialize(name,price)
-    @name = name
-    @price = price
+    self.new("water", 100)
   end
 end
 
 class VendingMachine
-  MONEY = [10, 50, 100, 500, 1000].freeze
+MONEY = [10, 50, 100, 500, 1000].freeze
   def initialize
     @slot_money = 0
     @sale_amount = 0
-    @stock = {cola: 5, redbull: 5, water: 5}
+    @stock = {"cola" => 5, "redbull" => 5, "water" => 5}
   end
 
   def current_slot_money
@@ -59,70 +59,29 @@ class VendingMachine
   end
 
   def return_money
-      puts @slot_money
-      @slot_money = 0
+    puts @slot_money
+    @slot_money = 0
   end
 
-  def cola_info
-    if @slot_money >= Drink.cola.price && self.current_stock[:cola] > 0
-    puts "コーラを購入できます"
-    return true
-    end
-  end
-
-  def buy_cola
-    if @slot_money >= Drink.cola.price && self.current_stock[:cola] > 0
-      puts "コーラを購入しました"
-      @sale_amount += Drink.cola.price
-      x = self.current_stock[:cola] - 1
-      self.current_stock[:cola] = x
-      return_change = @slot_money -= Drink.cola.price
-      puts "お釣りは#{return_change}円です。"
-      return true
+  def drink_check(drink) #Drink.cola のような形で引数を渡す
+    if @slot_money >= drink.price && @stock["#{drink.name}"] > 0
+      puts "#{drink.name}を購入できます。"
     else
-      puts "コーラを購入できません。"
+      puts "#{drink.name}を購入できませんでした。"
     end
   end
 
-  def redbull_info
-    if @slot_money >= Drink.redbull.price && self.current_stock[:redbull] > 0
-    puts "水を購入できます"
-    return true
-    end
-  end
-
-  def buy_redbull
-    if @slot_money >= Drink.redbull.price && self.current_stock[:redbull] > 0
-      puts "レッドブルを購入しました"
-      @sale_amount += Drink.redbull.price
-      x = self.current_stock[:redbull] - 1
-      self.current_stock[:redbull] = x
-      return_change = @slot_money -= Drink.redbull.price
-      puts "お釣りは#{return_change}円です。"
-      return true
+  def buy_drink(drink) # Drink.cola のような形で引数を渡す
+    name = drink.name
+    price = drink.price
+    if @slot_money >= price && @stock["#{drink.name}"] > 0
+        puts "#{name}を購入しました。"
+        @sale_amount += price
+        @stock["#{drink.name}"] -= 1
+        return_change = @slot_money -= price
+        puts "残高は#{return_change}円です。"
     else
-      puts "レッドブルを購入できません。"
-    end
-  end
-
-  def water_info
-    if @slot_money >= Drink.water.price && self.current_stock[:water] > 0
-    puts "水を購入できます"
-    return true
-    end
-  end
-
-  def buy_water
-    if @slot_money >= Drink.water.price && self.current_stock[:water] > 0
-      puts "水を購入しました"
-      @sale_amount += Drink.water.price
-      x = self.current_stock[:water] - 1
-      self.current_stock[:water] = x
-      return_change = @slot_money -= Drink.water.price
-      puts "お釣りは#{return_change}円です。"
-      return true
-    else
-      puts "水を購入できません。"
+      puts "#{name}を購入できません。"
     end
   end
 
